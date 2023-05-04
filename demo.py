bd203bfb35db24889547a756061c512c14a689dc
六尘 <18368387155@13.com>
2023-05-04 23:57:35 +0800

    更新

--- demo.py
+++ demo.py
@@ -1,8 +1,8 @@
 from appium import webdriver
 desired_caps={}
 desired_caps['platformName']='Android'
-desired_caps['platformVersion']='6.0'
-desired_caps['deviceName']='emulator-5554'
+# desired_caps['platformVersion']='3.0.0'
+desired_caps['deviceName']='RXD0221812006206'
 desired_caps['appPackage']='com.android.settings.Settings'
 
 driver=webdriver.Remote('http://localhost:4723/wd/hub',desired_caps)
---
+++ test_sample.py
@@ -0,0 +1,9 @@
+
+def test_case1():
+    print("case1")
+
+def setup_function():
+    print("资源准备：setup function")
+
+def teardown_function():
+    print("资源销毁：teardown function")
